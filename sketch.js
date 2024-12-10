8d1c4c7042681e0e99e1ac6d40214c378ab0b544
Mehatey <mehts818@newschool.edu>
2024-12-10 17:49:31 -0500

    added boilerplate code

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    
+    // Your drawing code will go here
+    ellipse(width/2, height/2, 50, 50);
+}
